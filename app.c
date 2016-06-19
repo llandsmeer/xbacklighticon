337d980d47e027819d17c30caef7a7d3402a841c
Lennart Landsmeer <lennart@landsmeer.email>
2016-06-20 00:48:12 +0200

    events

--- app.c
+++ app.c
@@ -1,4 +1,5 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "app.h"
@@ -25,10 +26,43 @@ void app_init(struct app * app) {
 }
 
 void app_run(struct app * app) {
-    pause();
+    xcb_generic_event_t * event;
+    while ((event = xcb_wait_for_event(app->xcb_connection))) {
+        switch (event->response_type & ~0x80) {
+            case XCB_EXPOSE:
+                printf("expose\n");
+                break;
+            case XCB_REPARENT_NOTIFY:
+                printf("reparent\n");
+                break;
+            case XCB_DESTROY_NOTIFY:
+                printf("destroy\n");
+                return;
+                break;
+            case XCB_CONFIGURE_NOTIFY:
+                printf("configure\n");
+                break;
+            case XCB_MAP_NOTIFY:
+                printf("notify\n");
+                break;
+            case XCB_UNMAP_NOTIFY:
+                printf("unmap\n");
+                break;
+            case XCB_MAPPING_NOTIFY:
+                printf("mapping\n");
+                break;
+            default:
+                printf("unknown event %d\n", event->response_type);
+        }
+        free(event);
+        if (app->has_error) {
+            return;
+        }
+    }
 }
 
 void app_dispose(struct app * app) {
+    xcb_disconnect(app->xcb_connection);
 }
 
 void app_init_error(struct app * app, const char * message) {
--- init/tray_icon_window.c
+++ init/tray_icon_window.c
@@ -1,14 +1,17 @@
 #include "app_init.h"
 
 void app_init_tray_icon_window(struct app * app) {
+    uint32_t values[] = {
+        0xffffffff,
+        XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_STRUCTURE_NOTIFY};
     app->tray_icon_window = xcb_generate_id(app->xcb_connection);
     xcb_create_window(app->xcb_connection, XCB_COPY_FROM_PARENT,
             app->tray_icon_window, app->screen->root,
             0, 0, 16, 16, 0,
             XCB_WINDOW_CLASS_INPUT_OUTPUT,
             app->screen->root_visual,
-            0,
-            0);
+            XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK,
+            values);
     xcb_map_window(app->xcb_connection, app->tray_icon_window);
     xcb_flush(app->xcb_connection);
 }
